1197a295abd8ed875a1450c9d3c44015b18b4a9e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2024-04-07 11:20:43 +0900

    chore: remove loaded_tohtml

--- init.lua
+++ init.lua
@@ -1171,7 +1171,6 @@ local function init()
   vim.g.loaded_matchit = 1
   vim.g.loaded_netrwPlugin = 1
   vim.g.loaded_rplugin = 1
-  vim.g.loaded_tohtml = 1
   vim.g.loaded_tutor = 1
 
   vim.filetype.add({
